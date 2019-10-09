600fc03e9661d857baf8097e0da963f49c5144be
digvijay17july@gmail.com <digvijay17july@gmail.com>
2019-10-09 13:25:12 +0530

    change code

--- main.go
+++ main.go
@@ -8,12 +8,18 @@ import (
 
 func main(){
 	app:= api.App{}
+	fmt.Print("Running 1")
 	app.Initialize()
+
+	fmt.Print("Running 2")
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
 		port = "8080"
 	}
+
+	fmt.Print("Running 3")
 	app.Run(":"+port)
 	
+	fmt.Print("Running 4")
 	
 }
